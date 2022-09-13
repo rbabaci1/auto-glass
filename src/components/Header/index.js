29c98070aefa16aa78ddeed75b9e7593a2907b4b
rbabaci1 <4rabah@gmail.com>
2022-09-13 15:08:04 -0700

    create a prototype for the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,28 @@
 import './header.scss';
 
 export default function Header() {
-  return <h2>Header in progress</h2>;
+  return (
+    <div className='header-wrapper'>
+      <div className='covid-alert'>
+        Local auto glass shops facing glass shortage.{' '}
+        <a
+          href='https://www.ksby.com/news/local-news/local-auto-glass-shops-facing-glass-shortage'
+          rel='noreferrer'
+          target='_blank'
+        >
+          Learn more
+        </a>
+      </div>
+
+      <div className='navigation' role='navigation' aria-label='main'>
+        <h1>Logo</h1>
+        <div className='site-nav-container'>
+          <a href='http://localhost:3000/'>HOME</a>
+          <a href='http://localhost:3000/'>ABOUT US</a>
+          <a href='http://localhost:3000/'>PRICE</a>
+          <a href='http://localhost:3000/'>CONTACT US</a>
+        </div>
+      </div>
+    </div>
+  );
 }
