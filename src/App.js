d3a28a9c0370049b953a3182f4573fef36c1bd25
rbabaci1 <4rabah@gmail.com>
2022-09-22 16:56:41 -0700

    fix caroussel

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Carousell from './components/Carousel';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 
@@ -7,7 +6,6 @@ function App() {
     <div className='App'>
       <Header />
       <MainContent />
-      <Carousell />
     </div>
   );
 }
