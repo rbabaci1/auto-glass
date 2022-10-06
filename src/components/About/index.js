ec2c4c92046343e92b5d0023092d8ad0ada131e0
rbabaci1 <4rabah@gmail.com>
2022-10-06 11:38:42 -0700

    design the about section structure

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,18 @@ import './about.scss';
 export default function About() {
   return (
     <div className='about-container'>
-      <h2>ABOUT SECTION</h2>
+      <div className='inner-container'>
+        <div className='about-txt'>
+          <h4>ABOUT QUICK FIX CAR GLASS</h4>
+        </div>
+
+        <div className='about-imgs'>
+          <img
+            src='https://images.unsplash.com/photo-1659115904528-8650d05527a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGF1dG8lMjBnbGFzc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60'
+            alt='test'
+          />
+        </div>
+      </div>
     </div>
   );
 }
