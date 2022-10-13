2c8202980fc5a888868bf8cd4c73a59e64f99cb8
rbabaci1 <4rabah@gmail.com>
2022-10-13 13:39:44 -0700

    adjust service card dimensions

--- src/components/GlassServices/index.js
+++ src/components/GlassServices/index.js
@@ -15,12 +15,12 @@ export default function GlassServices() {
           img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
         />
         <ServiceCard
-          title='Complete Windshield Replacement'
-          img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+          title='Big Rigs / RV / Trucks and Vans'
+          img='https://media.istockphoto.com/photos/glazier-using-tools-repairing-to-fix-crack-broken-windshield-on-the-picture-id806662286?b=1&k=20&m=806662286&s=170667a&w=0&h=rf9gJ2Bw3iz23oAtrylzrgkIEa3bo2f7LshanafL_fQ='
         />
         <ServiceCard
-          title='Complete Windshield Replacement'
-          img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+          title='Regulator Replacement'
+          img='https://media.istockphoto.com/photos/luxury-car-at-public-dealership-picture-id165653253?b=1&k=20&m=165653253&s=170667a&w=0&h=WpYJaySRn3CMCgVlETTDlazxX_j3G6_fRP5tmZt8jN8='
         />
       </div>
     </div>
