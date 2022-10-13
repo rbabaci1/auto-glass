44098c31cf4e5af4ac3387c69ed54b9b945044fa
rbabaci1 <4rabah@gmail.com>
2022-10-13 13:35:03 -0700

    .

--- src/components/GlassServices/index.js
+++ src/components/GlassServices/index.js
@@ -9,10 +9,20 @@ export default function GlassServices() {
         <span />
       </div>
 
-      <ServiceCard
-        title='Complete Windshield Replacement'
-        img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
-      />
+      <div className='cards'>
+        <ServiceCard
+          title='Complete Windshield Replacement'
+          img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+        />
+        <ServiceCard
+          title='Complete Windshield Replacement'
+          img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+        />
+        <ServiceCard
+          title='Complete Windshield Replacement'
+          img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+        />
+      </div>
     </div>
   );
 }
