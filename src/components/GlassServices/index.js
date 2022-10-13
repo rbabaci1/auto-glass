950e80151475d06ab8c3555f198b3bab7f9fcbe7
rbabaci1 <4rabah@gmail.com>
2022-10-13 13:31:38 -0700

    make the service card comp reusable

--- src/components/GlassServices/index.js
+++ src/components/GlassServices/index.js
@@ -9,7 +9,10 @@ export default function GlassServices() {
         <span />
       </div>
 
-      <ServiceCard />
+      <ServiceCard
+        title='Complete Windshield Replacement'
+        img='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
+      />
     </div>
   );
 }
--- src/components/ServiceCard/index.js
+++ src/components/ServiceCard/index.js
@@ -1,14 +1,11 @@
 import './card.scss';
 
-export default function ServiceCard() {
+export default function ServiceCard({ title, img }) {
   return (
     <div className='service-card'>
-      <img
-        src='https://media.istockphoto.com/photos/auto-glass-repair-amp-replacement-picture-id155432177?b=1&k=20&m=155432177&s=170667a&w=0&h=lW8rQEpJOK6pdcEYtOAh7p-mbpEyTXfeZw2JLjIbGxA='
-        alt='a'
-      />
+      <img src={img} alt='a' />
 
-      <h4>Complete Windshield Replacement</h4>
+      <h4>{title}</h4>
     </div>
   );
 }
